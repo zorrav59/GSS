42a36e67c76972dbd976af6086eaf56eeb4e6734
zorrav59 <zorrav59@gmail.com>
2025-10-29 17:41:29 +0800

    Remo offensive language from main content

--- app.js
+++ app.js
@@ -5,3 +5,13 @@ menu.addEventListener("click", function () {
   menu.classList.toggle("is-active");
   menuLinks.classList.toggle("active");
 });
+
+const gabron = document.querySelector("#mobile-menu");
+const dayat = document.querySelector(".navbar__menu");
+
+document.addEventListener("click", function (e) {
+  if (!gabron.contains(e.target) && !dayat.contains(e.target) ) {
+    gabron.classList.remove("is-active");
+    dayat.classList.remove("active");
+  }
+});
